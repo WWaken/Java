a086d25e09162fd46d8f1b8177ca03b2346c255e
WWaken <1549200803@qq.com>
2020-07-29 22:24:03 +0800

    69387-n个数里出现次数大于等于n/2的数

---
+++ Leetcode/src/$0729.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * @ClassName: $0729
+ * @Description:69387-n个数里出现次数大于等于n/2的数
+ * @Author: Ma Yuanyuan
+ */
+public class $0729 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String str = in.nextLine();
+            String[] strs = str.split(" ");
+            int[] arr = new int[strs.length];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = Integer.valueOf(strs[i]);
+            }
+            int num = arr[0];
+            int count = 0;
+            for (int j = 1; j < arr.length; j++) {
+                if (arr[j] == num) {
+                    count++;
+                } else if (count > 0) {
+                    count--;
+                } else {
+                    num = arr[j];
+                }
+            }
+            System.out.println(num);
+        }
+    }
+}
