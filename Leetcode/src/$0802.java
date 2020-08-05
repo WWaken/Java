f3e9cf13e31366e4374ab8cd5b50b6fd36efddeb
WWaken <1549200803@qq.com>
2020-08-05 22:10:24 +0800

    DNA序列

---
+++ Leetcode/src/$0802.java
@@ -0,0 +1,32 @@
+//import java.lang.reflect.Array;
+//import java.util.Arrays;
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.Scanner;
+//
+///**
+// * @ClassName: $0802
+// * @Description: 只出现一次的字符
+// * @Author: Ma Yuanyuan
+// */
+//public class $0802 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        while(sc.hasNext()){
+//            String str = sc.nextLine();
+//        }
+//    }
+//    public static int onlyOne(String str){
+//        if(str == null || str.length() ==0){
+//            return -1;
+//        }
+//        HashMap<Integer,Character> map = new HashMap<>();
+//        for (int i = 0; i < str.length(); i++) {
+//            if(map.containsKey(str.charAt(i))){
+//                int value = map.get(str.charAt(i));
+//                map.remove
+//            }
+//        }
+//
+//    }
+//}
